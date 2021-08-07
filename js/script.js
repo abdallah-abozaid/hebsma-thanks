740ddad19ae47c5256a36959a82ae3f4b838673e
abdallah abo zaid <abdallahabozaid1999@gmail.com>
2021-08-07 23:44:14 +0300

    thanks page for hebsma education

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+document.querySelector(".navbar-toggler").onclick = function () {
+    document.querySelector(".navbar-toggler span.sec-p").classList.toggle("open")
+}
+
+window.onload = function () {
+    document.querySelector(".loader ").classList.toggle("hide")
+}
